eed304edc4c1f354412e39e88914cb6efcad7d1a
MOHAMMED KABIR <mkkabr@amazon.com>
2023-10-31 15:24:21 +0000

    migrating from null-ls

--- lua/mekabir/packer.lua
+++ lua/mekabir/packer.lua
@@ -24,7 +24,11 @@ packer.startup(function(use)
 		-- or                            , branch = '0.1.x',
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
-	-----------------------------------Color Schemes-------------------------------
+
+    -- https://github.com/ThePrimeagen/harpoon
+    use({ "theprimeagen/harpoon" })
+
+-----------------------------------Color Schemes-------------------------------
 	-- setting  :colorscheme ron.
 	-- https://github.com/rose-pine/neovim
 	use({
@@ -46,7 +50,10 @@ packer.startup(function(use)
     use({
         "xiyaowong/transparent.nvim"
     })
-	-- the following is an example of translating from Plug to packer
+
+--------------------------------------- LSP ---------------------------------------
+
+    -- the following is an example of translating from Plug to packer
 	-- https://github.com/nvim-treesitter/nvim-treesitter
 	use({
 		"nvim-treesitter/nvim-treesitter",
@@ -56,47 +63,52 @@ packer.startup(function(use)
 	-- https://github.com/nvim-treesitter/playground
 	use({ "nvim-treesitter/playground" })
 
-	-- https://github.com/ThePrimeagen/harpoon
-	use({ "theprimeagen/harpoon" })
 	-- https://github.com/mbbill/undotree
 	use({ "mbbill/undotree" })
-	----------------------------------   git  -------------------------------------
-	-- https://github.com/tpope/vim-fugitive
+
+    -- https://github.com/williamboman/mason.nvim 
+    -- https://github.com/neovim/nvim-lspconfig
+    -- https://github.com/williamboman/mason-lspconfig.nvim
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+
+    -- https://github.com/VonHeikemen/lsp-zero.nvim
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            -- Uncomment these if you want to manage LSP servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
+    -- -- https://github.com/ryanoasis/vim-devicons
+
+----------------------------------   git  -------------------------------------
+
+    -- https://github.com/tpope/vim-fugitive
 	use({ "tpope/vim-fugitive" })
 	-- https://github.com/lewis6991/gitsigns.nvim
 	use({ "lewis6991/gitsigns.nvim" })
 
-	-- https://github.com/VonHeikemen/lsp-zero.nvim
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{ -- Optional
-				"williamboman/mason.nvim",
-				run = function()
-					pcall(vim.cmd, "MasonUpdate")
-				end,
-			},
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" }, -- Required
-			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-			{ "L3MON4D3/LuaSnip" }, -- Required
-			{ "sharkdp/fd" }, -- Required
-		},
-	})
-	-- -- https://github.com/preservim/nerdtree
-	-- use ({ 'preservim/nerdtree' })
 
-	-- -- https://github.com/ryanoasis/vim-devicons
 	-- use ({ 'ryanoasis/vim-devicons' })
 
 	-- -- https://github.com/Xuyuanp/nerdtree-git-plugin
 	-- use ({ 'Xuyuanp/nerdtree-git-plugin' })
 
+------------------------------ File System Navigation ------------------------------
 	-- https://github.com/nvim-tree/nvim-tree.lua
 	use({
 		"nvim-tree/nvim-tree.lua",
@@ -105,6 +117,9 @@ packer.startup(function(use)
 		},
 	})
 
-	-- https://github.com/jose-elias-alvarez/null-ls.nvim
-	use({ "jose-elias-alvarez/null-ls.nvim" })
+    -- -- https://github.com/preservim/nerdtree
+    -- use ({ 'preservim/nerdtree' })
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim (null-ls was archived 2023)
+	-- use({ "jose-elias-alvarez/null-ls.nvim" })
 end)
